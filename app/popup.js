47ba064ad71f7906f81f6976554857fa6d57068d
Alexander Libby <alibby@insourceservices.com>
2022-10-28 23:00:35 -0400

    finalized UI. Ready to bind events.

--- app/popup.js
+++ app/popup.js
@@ -1,28 +1,34 @@
 $(document).ready(function() {
-    $("#patch").change(setting_change);
     $("#push").change(setting_change);
     $("#sound").change(setting_change);
-    load_setting($("#patch"), "patch");
     load_setting($("#push"), "push");
     load_setting($("#sound"), "sound");
-    $.getJSON("https://insourceservices.zendesk.com/api/v2/views.json", function( data ) {
+
+    $.getJSON("https://insourceservices.zendesk.com/api/v2/views.json?active=true&sort_by=alphabetical", function( data ) {
         for(view in data.views) {
             if(data.views[view].active && data.views[view].restriction != null && data.views[view].restriction.type == "Group") {
-                let input = $("<input/>", { "class":"form-check-input "+data.views[view].id, "type":"checkbox", "id":"input_"+data.views[view].id});
-                let label = $("<label/>", {"class":"form-check-label ps-2 "+data.views[view].id, "for":"input_"+data.views[view].id, "text":data.views[view].title});
-                $("#views").append($("<li/>", {"class":"form-check-control form-switch my-1 py-1"}).append([input, label]));
+                
+                let input = $("<input/>", {"class":"form-check-input", "type":"checkbox", "id":"push"});
+                let link = $("<a/>", {"class":"form-check-label link-primary ps-2", "href":"https://insourceservices.zendesk.com/agent/filters/"+data.views[view].id, "text":data.views[view].title});
+                let count = $("<span/>", {"class":"badge bg-primary rounded-pill"})
+                $("#views2").append($("<li/>", {"class":"list-group-item d-flex justify-content-between align-items-center"}).append([
+                    $("<div/>", {"class":"form-switch"}).append([input, link]), count
+                ]));
                 input.change(setting_change);
-                set_ticket_count(label, data.views[view].id);
+                set_ticket_count(count, data.views[view].id);
                 load_setting(input, "input_"+data.views[view].id);
+                $(link).click(function(){
+                    chrome.tabs.create({url:$(this).attr('href')});
+                });
             }
         }
     });
 
 function set_ticket_count(element, viewId) {
-    $.getJSON("https://insourceservices.zendesk.com/api/v2/views/"+viewId+"/tickets.json", function( ticketView ) {
+    $.getJSON("https://insourceservices.zendesk.com/api/v2/views/"+viewId+"/count.json", function( ticketView ) {
         var checkExist = setInterval(function() {
             if (element.length) {
-               element.append(" ("+ticketView.count+")");
+               element.append(""+ticketView.view_count.value+"");
                clearInterval(checkExist);
             }
         }, 10);
@@ -43,4 +49,16 @@ function setting_change() {
     save[$(this).attr('id')] = $(this).is(':checked');
     console.log("Save", save);
     chrome.storage.sync.set(save);
+
+    chrome.notifications.create('test', {
+        type: 'basic',
+        iconUrl: 'images/Zendesk.png',
+        title: 'Test Message',
+        message: 'You are awesome!',
+        priority: 2
+    });
+
+    //var notification = new Audio(chrome.runtime.getURL("/sounds/E6thChord.wav"));
+    //notification.volume = .05;
+    //notification.play();
 }
--- scripts/content.js
+++
@@ -1,4 +0,0 @@
-console.log("Test Code");
-var els = document.querySelectorAll('a[href*="/agent/filters/"]');
-
-console.log(els)
